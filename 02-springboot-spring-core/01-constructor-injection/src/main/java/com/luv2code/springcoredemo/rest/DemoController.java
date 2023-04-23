1147b2d0ed06e7ccd8113112694515ff05617ec1
yc-98 <yashchandarana86@gmail.com>
2023-04-23 14:35:05 +0530

    learn use of qualifier annotation when multiple beans are available for injection

--- 02-springboot-spring-core/01-constructor-injection/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
+++ 02-springboot-spring-core/01-constructor-injection/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -2,6 +2,7 @@ package com.luv2code.springcoredemo.rest;
 
 import com.luv2code.springcoredemo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +11,7 @@ public class DemoController {
     private Coach myCoach;
 
     @Autowired
-    public void setCoach(Coach theCoach){
+    public void DemoController(@Qualifier("baseballCoach") Coach theCoach){
         myCoach=theCoach;
     }
     @GetMapping("/dailyworkout")
